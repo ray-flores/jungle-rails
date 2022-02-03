2bca355e4e85ce23de497caf4601114fb38cb7ae
Ray Flores <ray.m.flores@gmail.com>
2022-02-03 19:54:49 +0000

    Add feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.count(:id)
+    @category_count = Category.count(:id)
   end
 end
+
+
+
+# @category = Category.find(params[:id])
+# @products = @category.products.order(created_at: :desc)
