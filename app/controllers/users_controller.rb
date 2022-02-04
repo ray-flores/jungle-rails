71c14b97aab10181b7c9bb8f9b29fb0228cbcdb5
Ray Flores <ray.m.flores@gmail.com>
2022-02-04 22:56:29 +0000

    Add login/logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
+
   def new
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user-id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
+
 end
