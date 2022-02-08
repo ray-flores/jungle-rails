8ae56d6c3c34e149e58795a75fafe905249f2054
Ray Flores <ray.m.flores@gmail.com>
2022-02-08 00:18:17 +0000

    Create first test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
+    context 'given all valid fields' do
+      it 'saves successfully' do
+        @category = Category.new(name: 'Vehicles')
+        @product = Product.new(name: 'Toyota Camry', price: 50000, quantity: 1, category: @category)
+      end
+    end
+
     
 
   end
