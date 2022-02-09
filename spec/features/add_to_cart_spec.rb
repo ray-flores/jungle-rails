0edefd951a5fdcb6e254c7ba8d24628d18b73412
Ray Flores <ray.m.flores@gmail.com>
2022-02-09 02:28:51 +0000

    Add feature spec for add to cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-  
   #SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,19 +16,17 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They click on a product to see its details" do
+  scenario "They add a product and increase the cart by one" do
     # ACT
     visit root_path
-    first(".btn.btn-default").click
-    # visit '/products/10'
+    first(".btn.btn-primary").click
+
     # DEBUG 
     puts page.html
     save_screenshot
     #save_and_open_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product-detail'
+    expect(page).to have_content 'My Cart (1)'
   end
-
-
 end
